1ddc1338b9eda82c3b83ab6504943f4cdcc1bccc
Jordan Cooper <jordan@Jordans-MacBook-Air.local>
2021-09-03 13:04:00 +1200

    fill out QuizPage

--- client/components/QuizPage.jsx
+++ client/components/QuizPage.jsx
@@ -1,12 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Question from './Question'
 import Answer from './Answer'
+import Compliment from './Compliments'
+import Insult from './Insults'
+
+import request from 'superagent'
+
+// UNWANTED SHIT
+// /api/v1/questions/3
 
 function Quiz () {
+  const [quest, setQuest] = useState({
+    "id": 3,
+    "question": "Loading...",
+    "possibleAnswers": [
+      "Loading.",
+      "Loading..",
+      "Loading...",
+      "Loading...."
+    ],
+    "correctAnswer": "Not an answer!!"
+  })
+
+  const getQuestion = () => {
+    const randomNumber = Math.floor(Math.random() * 10 + 1)
+    return request.get(`/api/v1/questions/${randomNumber}`)
+      .then (response => {
+        return response.body
+      })
+      .then (response => {
+        setQuest(response)
+      })
+  }
+
+  useEffect(() => {
+    // refreshQuestion()
+    getQuestion()
+  }, [])
+  
+  const refreshQuestion = () => {
+    getQuestion()
+      // .then(nextQuestion => setQuest(nextQuestion))
+  }
+
   return (
     <div>
-        <h1>Quiz Page</h1>
+      <h1>Quiz Page</h1>
+      <Question q={quest.question}/>
+      <Answer a={quest.possibleAnswers} ca={quest.correctAnswer} next={refreshQuestion} />
+      <Compliment />
+      <Insult />
+      <button onClick={refreshQuestion}> Next Question </button>
     </div>
   )
 }
