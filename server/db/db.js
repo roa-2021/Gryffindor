e84396a31622a12b39f42163e93e5c8a3f05d8fa
Jordan Cooper <jordan@Jordans-MacBook-Air.local>
2021-09-03 15:20:33 +1200

    push to main 🤜

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,5 @@
-const knex = require('knex')(config)
-const config = require('./knexfile')[env]
+const knex = require('knex')
+const config = require('./knexfile')
 const env = process.env.NODE_ENV || 'development'
 const connection = knex(config[env])
 
